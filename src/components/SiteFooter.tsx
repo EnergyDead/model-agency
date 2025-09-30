44dba65d77cc5dbc2647a1d5f9400d9187ee1db8
Ruslan V <rusvas012@gmail.com>
2025-09-30 16:02:35 +0400

    init 3

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -5,15 +5,14 @@ export default function SiteFooter() {
         <footer className="footer" role="contentinfo">
             <div className="footer__inner">
                 <ul className="footer__list">
-                    <li><span className="footer__label">Instagram:</span> <a href="https://www.instagram.com/serendipite.art"
-                                                                             target="_blank"
-                                                                             rel="noreferrer">@serendipite.art</a></li>
-                    <li><span className="footer__label">Telegram:</span> <a href="https://t.me/srndpte"
-                                                                            target="_blank"
-                                                                            rel="noreferrer">@srndpte</a></li>
-                    <li><span className="footer__label">Gmail:</span> <a
-                        href="mailto:temp@gmail.com">temp@gmail.com</a></li>
-
+                    <li><span className="footer__label">Instagram:</span> <a
+                        href="https://www.instagram.com/serendipite.art"
+                        target="_blank"
+                        rel="noreferrer">@serendipite.art</a></li>
+                    <li><span className="footer__label">Telegram:</span> <a
+                        href="https://t.me/srndpte"
+                        target="_blank"
+                        rel="noreferrer">@srndpte</a></li>
                     <div className="footer__name">sérendipité.</div>
                 </ul>
             </div>
